c17eb40aecb59f595f15af3b053bfedc570f2781
735416909@qq.com <735416909@qq.com>
2016-09-30 21:52:39 +0800

    add webhook

---
+++ web/webhook.php
@@ -0,0 +1,26 @@
+<?php 
+
+	$key = 'myapp' ;
+
+	$method = $_REQUEST ['METHOD_NAME'] ; 
+
+	if ( 'POST' !== strtoupper($method)) {
+		exit('NO ACCESS DENY ');
+	}
+
+
+	$post = json_decode ( $_POST ) ;
+
+	if(empty($post ['config'])) {
+		exit('NO ACCESS DENY ');
+	}
+
+	$git_key = $post ['config'] ['secret'] ; 
+
+	if($git_key != $key){
+		exit('NO ACCESS DENY ');
+	}
+
+	if(function_exists('exec')){
+		exec('cd /home/wwwroot/a.zhangmj.com/yiishop/ && git pull') ;
+	}
